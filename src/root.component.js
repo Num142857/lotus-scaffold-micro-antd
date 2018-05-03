d52a81696e36d1174dc5036cd8912e6d66876c24
杨帆 <incomparable9527@foxmail.com>
2018-05-03 13:25:59 +0800

    去掉无用代码

--- src/root.component.js
+++ src/root.component.js
@@ -5,35 +5,14 @@ import BasicLayout from './layouts/BasicLayout'
 import UserLayout from './layouts/UserLayout'
 import { BrowserRouter, HashRouter, Route, hashHistory, Switch, Redirect } from 'react-router-dom'
 import { getRouterData } from './common/router'
-import createHistory from 'history/createBrowserHistory'
 import axios from './utils/request'
-const history = createHistory()
 
-// Get the current location.
-const location = history.location
-
-// Listen for changes to the current location.
-const unlisten = history.listen((location, action) => {
-  // location is an object like window.location
-  console.log('路由更新了', action, location.pathname, location.state)
-})
-
-// Use push, replace, and go to navigate around.
-// history.push('/home', { some: 'state' })
-
-// To stop listening, call the function returned from listen().
-// unlisten()
 export default class RootComponent extends React.Component {
     state = { store: this.props.store, globalEventDistributor: this.props.globalEventDistributor };
 
     componentDidCatch(error, info) {
       console.log(error, info)
     }
-    async componentWillMount() {
-      let res = await axios.get('/api/fake_chart_data')
-      console.log(res)
-    }
-
     setStore(store) {
       this.setState({ ...this.state, store: store })
     }
