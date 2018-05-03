06a93f8fb551a1b41a386861c90ff6fe9ba84acc
杨帆 <incomparable9527@foxmail.com>
2018-05-03 14:20:08 +0800

    fixed

--- src/root.component.js
+++ src/root.component.js
@@ -30,7 +30,7 @@ export default class RootComponent extends React.Component {
       let store = this.state.globalEventDistributor.getState()
       let menu = []
       Object.keys(store).forEach((name) => {
-        pushStore(store[name].menu)
+        store[name].menu && pushStore(store[name].menu)
       })
       if (this.state.store && this.state.globalEventDistributor) {
         ret = <Provider store={this.state.store}>
